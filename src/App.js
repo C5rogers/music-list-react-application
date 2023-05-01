f2e80abc710f0f348930b02401fcb18502343e33
c5rogers <natenaelhaylemariam@gmail.com>
2023-05-01 13:35:16 -0400

    music-list-website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import MyMusics from './components/MyMusics'
+import Create from './components/Create'
+import { Routes,Route } from 'react-router-dom';
+import Edit from './components/Edit'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/myMusics' element={<MyMusics />} />
+          <Route path='/create' element={<Create />} />
+          <Route path='/edit/:id' element={<Edit />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
